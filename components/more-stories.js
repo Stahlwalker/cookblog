5337ed27c85c087f5270de907b96783f72abb5eb
Lucas Stahl <30933416+Stahlwalker@users.noreply.github.com>
2022-02-01 14:30:14 -0600

    updated query

--- components/more-stories.js
+++ components/more-stories.js
@@ -4,7 +4,7 @@ export default function MoreStories({ posts }) {
   return (
     <section>
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
+        More Recipes
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
         {posts.map((post) => (
