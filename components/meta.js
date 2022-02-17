115d279e0b9c809ca07b001c82465da3daa68950
Lucas Stahl <30933416+Stahlwalker@users.noreply.github.com>
2022-02-17 09:08:36 -0600

    updating open graph whole website

--- components/meta.js
+++ components/meta.js
@@ -45,14 +45,6 @@ export default function Meta() {
       <meta name="twitter:description" content="A blog dedicated to cooking up recipes for all those far far and away." />
       <meta property="twitter:image" content="https://cookblog.vercel.app/images/starwarssocialfinal.jpg"/>
 
-  
-      {/* <meta content="51134710437125" property="fb:app_id" /> */}
-      {/* <meta content="Stahlwalker" property="og:site_name" />
-      <meta content="Stahlwalker" property="og:title" />
-      <meta content="article" property="og:type" />
-      <meta content="Web design, digital marketing and a tech blog for web programmers." property="og:description" />
-      <meta content="https://stahlwalker.org/" property="og:url" />
-      <meta content="https://cdn.dribbble.com/users/571141/screenshots/3549732/untitled-1_1x.jpg" property="og:image" /> */}
 
       <meta property="og:title" content="Stahlwalker Cookbook"/>
       <meta property="og:type" content="article"/>
