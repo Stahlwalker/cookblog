a620b2d8932d35907d5139ad5908d6ffbe93f6f3
Lucas Stahl <30933416+Stahlwalker@users.noreply.github.com>
2022-02-27 10:51:45 -0600

    attempted build-search fix

--- scripts/build-search.js
+++ scripts/build-search.js
@@ -1,23 +1,94 @@
 // build-search.js
 const dotenv = require("dotenv");
-import { getAllPostsForHome } from '/../lib/api'
+const algoliasearch = require("algoliasearch/lite");
+const fetch = require("node-fetch");
 
-
-export async function getAllPostsForHome(preview) {
-    const entries = await fetchGraphQL(
-      `query {
-        postCollection(order: date_DESC, preview: ${preview ? 'true' : 'false'}, limit: 0) {
-          items {
-            ${POST_GRAPHQL_FIELDS}
-          }
-        }
-      }`,
-      preview
-    )
-    return extractPostEntries(entries)
+const POST_GRAPHQL_FIELDS = `
+slug
+title
+coverImage {
+  url
+}
+date
+foodCategory
+author {
+  name
+  picture {
+    url
   }
+}
+excerpt
+content {
+  json
+  links {
+    assets {
+      block {
+        sys {
+          id
+        }
+        url
+        description
+      }
+    }
+  }
+}
+`
+
+// async function getAllPosts() {
+//   // write your code to fetch your data
+//   let shouldQueryMorePosts = true;
+//   const returnPosts = [];
+
+//   while (shouldQueryMorePosts) {
+//     const response = await getPaginatedPosts(page);
+
+//     if (response.posts.length > 0) {
+//       returnPosts.push(...response.posts);
+//     }
+
+//     shouldQueryMorePosts = returnPosts.length < response.total;
+//     page++;
+//   }
+
+//   return returnPosts;
+// }
+
+async function fetchGraphQL(query, preview = false) {
+  return fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${
+          preview
+            ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+            : process.env.CONTENTFUL_ACCESS_TOKEN
+        }`,
+      },
+      body: JSON.stringify({ query }),
+    }
+  ).then((response) => response.json())
+}
+
+async function getAllPostsForHome(preview) {
+  const entries = await fetchGraphQL(
+    `query {
+      postCollection(order: date_DESC, preview: ${preview ? 'true' : 'false'}, limit: 0) {
+        items {
+          ${POST_GRAPHQL_FIELDS}
+        }
+      }
+    }`,
+    preview
+  )
+  return extractPostEntries(entries)
+}
+
+
 
 function transformPostsToSearchObjects(posts) {
+  // ...
   const transformed = posts.map((post) => {
     return {
       objectID: post.sys.id,
@@ -37,11 +108,27 @@ function transformPostsToSearchObjects(posts) {
   dotenv.config();
 
   try {
-    const posts = await getAllBlogPosts();
+    const posts = await getAllPostsForHome(preview);
     const transformed = transformPostsToSearchObjects(posts);
 
-    // we have data ready for Algolia!
-    console.log(transformed);
+    // initialize the client with your environment variables
+    const client = algoliasearch(
+       process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
+       process.env.ALGOLIA_SEARCH_ADMIN_KEY,
+     );
+
+     // initialize the index with your index name
+     const index = client.initIndex("my_awesome_content");
+
+     // save the objects!
+     const algoliaResponse = await index.saveObjects(transformed);
+
+     // check the output of the response in the console
+     console.log(
+       `🎉 Sucessfully added ${algoliaResponse.objectIDs.length} records to Algolia search. Object IDs:\n${algoliaResponse.objectIDs.join(
+         "\n",
+       )}`,
+     );
   } catch (error) {
     console.log(error);
   }
