87f9993101fda63512cb09d57e6b82b62a9b9a62
Lucas Stahl <30933416+Stahlwalker@users.noreply.github.com>
2022-03-13 10:50:45 -0500

    fixed toggle nav button

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -54,7 +54,7 @@ export default function Post({ post, morePosts, preview }) {
               <Head>
               <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet"></link>
               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-              <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
+              {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" /> */}
                 <title>
                   {post.title} | Stahlwalker Cookbook
                 </title>
