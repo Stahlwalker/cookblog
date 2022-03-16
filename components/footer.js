56392a2166101717ab810b37bbd6fbde12b91959
Lucas Stahl <30933416+Stahlwalker@users.noreply.github.com>
2022-03-16 07:46:34 -0500

    style clean up

--- components/footer.js
+++ components/footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="https://stahlwalker.org/"
-              className="mx-3 bg-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-1 bg-black border border-dark text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Learn More
             </a>
