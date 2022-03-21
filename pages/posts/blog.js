e4efc92f334fe5d1db5fc56b72ca9b5dfeb51d27
Lucas Stahl <30933416+Stahlwalker@users.noreply.github.com>
2022-03-20 19:53:40 -0500

    another attemped pagination

--- pages/posts/blog.js
+++ pages/posts/blog.js
@@ -2,14 +2,18 @@ import Container from '../../components/container'
 import MoreBlog from '../../components/more-blog'
 import HeroPost from '../../components/hero-post'
 import Layout from '../../components/layout'
-import { getAllPostsForHome } from '../../lib/api'
+import { getAllPostsForHome, getPreviewPostBySlug } from '../../lib/api'
 import Head from 'next/head'
 import SimpleLayout from '../../components/layout/simple'
 
 
 
-export default function Blog({ preview, allPosts }) {
-  const heroPost = allPosts[0]
+export default function Blog({ preview, allPosts, page, total }) {
+  // const heroPost = allPosts[0]
+  const POSTS_PER_PAGE = 10;
+  // const page = useState(0);
+  const hasNextPage = Math.ceil(total / POSTS_PER_PAGE) > page;
+  const hasPreviousPage = page > 1;
   return (
     <>
       <Layout preview={preview}>
@@ -26,13 +30,64 @@ export default function Blog({ preview, allPosts }) {
           {allPosts.length > 0 && <MoreBlog posts={allPosts} showMore={false} />}
         </Container>
       </Layout>
+     
+          {allPosts && (
+        <>
+          {hasPreviousPage && (
+            <Link href={`/posts/blog/${page - 1}`}>
+              Previous {page - 1}/{total}
+            </Link>
+          )}
+          {hasNextPage && (
+            <Link href={`/posts/blog${page + 1}`}>
+              Next {page + 1}/{total}
+            </Link>
+          )}
     </>
-  )
-}
+    )}
+    </>
+  );
+};
 
-export async function getStaticProps({ preview = false }) {
-  const allPosts = (await getAllPostsForHome(preview)) ?? []
+// export const getStaticPaths = async () => {
+//   const allPosts = (await getPreviewPostBySlug()) ?? []
+//   return {
+//     paths: allPosts?.map(({ slug }) => `/posts/blog/${slug}`) ?? [],
+//     fallback: true,
+//   }
+// }
+
+export const getStaticPaths = async () => {
+  // Get total pages
+  const POSTS_PER_PAGE = 10;
+  const pages = Math.ceil(getPreviewPostBySlug.length / POSTS_PER_PAGE);
+  // Generate paths for Next.js. It is like: [{ params: { page: 1 } }, { params: { page: 2 }},...]
+  const paths = Array.from(Array(pages).keys()).map((page) => ({
+    params: { page: String(page + 1) },
+  }));
+
   return {
-    props: { preview, allPosts },
-  }
-}
+    paths,
+    fallback: true,
+  };
+};
+
+
+export const getStaticProps = async ({ params }) => {
+  const { page } = params;
+  const { posts, total } = (await getAllPostsForHome(preview)) ?? [];
+  // const allPosts = (await getAllPostsForHome(preview)) ?? []
+  return {
+    // props: { preview, allPosts },
+    props: {
+    posts: posts.slice((page - 1) * POSTS_PER_PAGE, page * POSTS_PER_PAGE),
+    page: Number(page),
+    total,
+  },
+  };
+};
+
+// export const getStaticProps = async ({ params }) => {
+//   const { page } = params;
+//   const { posts, total } = await getPosts();
+
