8237566a768246f0b18f3f2256656ecec3751a1d
Lucas Stahl <30933416+Stahlwalker@users.noreply.github.com>
2022-07-29 11:35:50 -0500

    created dark mode

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Nav, Button } from 'react-bootstrap';
 import Link from 'next/link'
 
 export default function Navbar() {
     // const currentUrl = window.location.pathname;
+
+    const toggleDarkMode = () => {
+        document.body.classList.toggle('light-mode')
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/"><i aria-hidden className="fa fa-home" style={{ color: "white", fontSize: 36 }}></i></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                <div className="nav-icon-contatiner">
+                    <div className="nav-icons">
+                        <a className="navbar-brand" href="/"><i aria-hidden className="fa fa-home" style={{ color: "white", fontSize: 36 }}></i></a>
+                        <i aria-hidden className="fa fa-lightbulb" style={{ color: "white", fontSize: 30 }} onClick={toggleDarkMode}></i>
+                    </div>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                </div>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
